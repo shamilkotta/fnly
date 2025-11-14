b293fbb0d9c3ce38ef2455744fe47bc661dad5a1
shamilkotta <shamilkotta99@gmail.com>
2025-11-14 07:54:05 +0530

    chore: apps merge

--- apps/example/api/_utils.ts
+++
@@ -1,3 +0,0 @@
-export const wish = () => {
-  return "Hello, dev server";
-};
--- apps/example/api/sub/index.ts
+++
@@ -1,8 +0,0 @@
-import { nanoid } from "nanoid";
-
-export async function GET() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello sub root route!", id: nanoid() })
-  };
-}
